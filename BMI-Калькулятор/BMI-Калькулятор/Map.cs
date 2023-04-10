078abcebf79c82d3a75df0ff0325c19cdf5d7ac0
Александр Ковяров <68403305+setup663@users.noreply.github.com>
2023-04-10 14:44:42 +0300

    Add files via upload

---
+++ BMI-Калькулятор/BMI-Калькулятор/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Калькулятор
+{
+    public partial class Map : Form
+    {
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("da");
+        }
+    }
+}
